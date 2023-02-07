8e3b3a6b31d7ecd12d2d4a88d97b667803385010
Avinash <palakurthiavinash07@gmail.com>
2023-02-07 08:50:52 +0530

    problem 1

--- 01-problem.js
+++ 01-problem.js
@@ -1,1 +1,17 @@
-//!1. Explain what a callback function is and provide a simple example
+// source: https://www.fullstack.cafe/blog/javascript-code-interview-questions
+
+//? 1. Explain what a callback function is and provide a simple example
+
+// Answer :  A callback function is a function that is passed to another function as an argument and is executed after some operation has been completed.
+
+// Below is an example of a simple callback function that logs to the console after some operations have been completed.
+
+const modifyArray = (arr, callback) => {
+  arr.push(100);
+  callback();
+};
+let arr = [1, 2, 3, 4];
+
+modifyArray(arr, function () {
+  console.log("array changed", arr);
+});
